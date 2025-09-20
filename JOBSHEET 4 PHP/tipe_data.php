795d264d252aa13f41cd1a659ba273b9489a68b6
martaaa-16 <martaprama@gmail.com>
2025-09-20 14:36:26 +0700

    praktikum 2 langkah 14

--- JOBSHEET 4 PHP/tipe_data.php
+++ JOBSHEET 4 PHP/tipe_data.php
@@ -49,4 +49,9 @@ echo "Nama Depan: {$namaDepan} <br>";
 echo 'Nama Belakang: ' . $namaBelakang . '<br>';
 
 echo $namaLengkap;
+
+echo "<br>";
+echo "<br>";
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo $listMahasiswa[0];
 ?>
