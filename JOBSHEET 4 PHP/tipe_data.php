5ca5f1feae05b0c17e5cf69642b46fc26e33baaa
martaaa-16 <martaprama@gmail.com>
2025-09-20 14:31:31 +0700

    praktikum 2 langkah 8

--- JOBSHEET 4 PHP/tipe_data.php
+++ JOBSHEET 4 PHP/tipe_data.php
@@ -19,10 +19,21 @@ $nilaiBahasaIndonesia = 9.3;
 
 $rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
 
+echo "<br>";
+echo "<br>";
 echo "Matematika: {$nilaiMatematika} <br>";
 echo "IPA: {$nilaiIPA} <br>";
 echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
 echo "Rata-rata: {$rataRata} <br>";
 
 var_dump($rataRata);
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+echo "<br>";
+echo "<br>";
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
 ?>
