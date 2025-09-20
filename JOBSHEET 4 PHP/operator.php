4951a8ae34a0e01221a9ce5d9a871e5944ad1996
martaaa-16 <martaprama@gmail.com>
2025-09-20 15:26:19 +0700

    perbaikan true/false pada praktikum 3

--- JOBSHEET 4 PHP/operator.php
+++ JOBSHEET 4 PHP/operator.php
@@ -26,13 +26,12 @@ $hasilLebihKecilSama = $a <= $b;
 $hasilLebihBesarSama = $a >= $b;
 
 echo "<br>";
-echo "<br>";
-echo "Hasil Sama: {$hasilSama} <br>";
-echo "Hasil Tidak Sama: {$hasilTidakSama} <br>";
-echo "Hasil Lebih Kecil: {$hasilLebihKecil} <br>";
-echo "Hasil Lebih Besar: {$hasilLebihBesar} <br>";
-echo "Hasil Lebih Kecil Sama: {$hasilLebihKecilSama} <br>";
-echo "Hasil Lebih Besar Sama: {$hasilLebihBesarSama} <br>";
+echo "Hasil Sama: " . ($hasilSama ? 'true' : 'false') . "<br>";
+echo "Hasil Tidak Sama: " . ($hasilTidakSama ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Kecil: " . ($hasilLebihKecil ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Besar: " . ($hasilLebihBesar ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Kecil Sama: " . ($hasilLebihKecilSama ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Besar Sama: " . ($hasilLebihBesarSama ? 'true' : 'false') . "<br>";
 
 //soal no 3.3
 $hasilAnd = $a && $b;
@@ -41,11 +40,10 @@ $hasilNotA = !$a;
 $hasilNotB = !$b;
 
 echo "<br>";
-echo "<br>";
-echo "Hasil And: {$hasilAnd} <br>";
-echo "Hasil Or: {$hasilOr} <br>";
-echo "Hasil Not A: {$hasilNotA} <br>";
-echo "Hasil Not B: {$hasilNotB} <br>";
+echo "Hasil And: " . ($hasilAnd ? 'true' : 'false') . "<br>";
+echo "Hasil Or: " . ($hasilOr ? 'true' : 'false') . "<br>";
+echo "Hasil Not A: " . ($hasilNotA ? 'true' : 'false') . "<br>";
+echo "Hasil Not B: " . ($hasilNotB ? 'true' : 'false') . "<br>";
 
 //soal no 3.4
 $penugasanHasilTambah = $a += $b;
@@ -54,7 +52,6 @@ $penugasanHasilKali = $a *= $b;
 $penugasanHasilBagi = $a /= $b;
 $penugasanSisaBagi = $a %= $b;
 
-echo "<br>";
 echo "<br>";
 echo "Penugasan Hasil Tambah: {$penugasanHasilTambah} <br>";
 echo "Penugasan Hasil Kurang: {$penugasanHasilKurang} <br>";
@@ -67,7 +64,6 @@ $hasilIdentik = $a === $b;
 $hasilTidakIdentik = $a !== $b;
 
 echo "<br>";
-echo "<br>";
-echo "Hasil Identik: {$hasilIdentik} <br>";
-echo "Hasil Tidak Identik: {$hasilTidakIdentik} <br>";
+echo "Hasil Identik: " . ($hasilIdentik ? 'true' : 'false') . "<br>";
+echo "Hasil Tidak Identik: " . ($hasilTidakIdentik ? 'true' : 'false') . "<br>";
 ?>
