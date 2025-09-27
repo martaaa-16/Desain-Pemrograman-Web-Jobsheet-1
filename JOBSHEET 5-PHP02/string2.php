c4f7cd1707bedd69b17bca95eaa0b54d44248924
martaaa-16 <martaprama@gmail.com>
2025-09-27 12:17:17 +0700

    soal no 11

---
+++ JOBSHEET 5-PHP02/string2.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Baris\nbaru<br>";
+echo 'Baris\nbaru<br>';
+echo "Halo\rDunia<br>";
+echo 'Halo\rDunia<br>';
+
+echo "<pre>Halo\tDunia!</pre>";
+echo '<pre>Halo\tDunia!</pre>';
+
+echo "Katakanlah\tTidak pada narkoba!<br>";
+echo 'Katakanlah\tTidak pada narkoba!<br>';
+
+?>
