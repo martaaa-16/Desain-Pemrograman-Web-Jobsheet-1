750622f84dfcceb731623e42234e960c35aa1a25
martaaa-16 <martaprama@gmail.com>
2025-09-27 11:09:34 +0700

    soal no 1

---
+++ JOBSHEET 5-PHP02/fungsi.php
@@ -0,0 +1,13 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalan, nama saya Marta<br>";
+    echo "Senang berkenalan dengan Anda<br>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+
+?>
