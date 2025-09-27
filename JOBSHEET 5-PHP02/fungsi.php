234c1bbd9969d953623d2b209386b3e32f9b6582
martaaa-16 <martaprama@gmail.com>
2025-09-27 11:27:14 +0700

    soal no 4

--- JOBSHEET 5-PHP02/fungsi.php
+++ JOBSHEET 5-PHP02/fungsi.php
@@ -1,20 +1,11 @@
 <?php
 
 //membuat fungsi
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
 
-//memanggil fungsi yang sudah dibuat
-perkenalan("Hamdan", "Hallo");
-
-echo "<hr>";
-
-$saya = "Marta";
-$ucapanSalam = "Selamat pagi";
-
-//memanggil lagi
-perkenalan($saya);
+echo "Umur saya adalah " . hitungUmur(2005, 2025) . " tahun";
 ?>
+
