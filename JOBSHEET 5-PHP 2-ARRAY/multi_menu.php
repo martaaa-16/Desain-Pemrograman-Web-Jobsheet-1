238d39fb405deda00fe5b7c198e1f17cfebac251
martaaa-16 <martaprama@gmail.com>
2025-09-28 11:04:51 +0700

    soal no 12

--- JOBSHEET 5-PHP 2-ARRAY/multi_menu.php
+++ JOBSHEET 5-PHP 2-ARRAY/multi_menu.php
@@ -35,8 +35,15 @@ $menu = [
 
 function tampilkanMenuBertingkat(array $menu) {
     echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li>{$item['nama']}</li>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+        
+        // Jika ada subMenu, panggil diri sendiri
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+        
+        echo "</li>";
     }
     echo "</ul>";
 }
