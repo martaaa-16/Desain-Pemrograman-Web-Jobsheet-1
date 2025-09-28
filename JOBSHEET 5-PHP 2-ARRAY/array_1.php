bf847353e466bf1ea14114f87f0e05f549c994b4
martaaa-16 <martaprama@gmail.com>
2025-09-28 10:16:49 +0700

    soal no 1 menggunakan loop

--- JOBSHEET 5-PHP 2-ARRAY/array_1.php
+++ JOBSHEET 5-PHP 2-ARRAY/array_1.php
@@ -7,9 +7,9 @@
 <?php
     $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 
-    echo $Listdosen[2] . "<br>";
-    echo $Listdosen[1] . "<br>";
-    echo $Listdosen[0] . "<br>";
+    for ($i = count($Listdosen) - 1; $i >= 0; $i--) {
+        echo $Listdosen[$i] . "<br>";
+    }
 ?>
 </body>
 </html>
